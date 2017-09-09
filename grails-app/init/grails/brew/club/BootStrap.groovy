b3c793c5f70da08510691cfe294c0082078808c6
Yago Lima <yagotoledolima@gmail.com>
2017-09-09 18:30:14 -0300

    Adicionados roles iniciais

--- grails-app/init/grails/brew/club/BootStrap.groovy
+++ grails-app/init/grails/brew/club/BootStrap.groovy
@@ -1,9 +1,20 @@
 package grails.brew.club
 
+import br.com.gbc.auth.Cargo
+
 class BootStrap {
 
     def init = { servletContext ->
+        // Cargos do sistema:
+        [
+            'ROLE_ADMIN',
+            'ROLE_USER'
+        ].each {
+            if(!Cargo.findByAuthority(it))
+                new Cargo(authority: it).save(flush: true, failOnError: true)
+        }
     }
+
     def destroy = {
     }
 }
